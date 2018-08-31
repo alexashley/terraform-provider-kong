5416c76070159bb2ffa8381d57b0a23be8989cdc
Alex Ashley <alexashley15@gmail.com>
2018-08-30 22:02:09 -0500

    Remove comments

--- kong/provider/resource_kong_service.go
+++ kong/provider/resource_kong_service.go
@@ -5,51 +5,24 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-//type KongService struct {
-//	id       string
-//	name     string
-//	protocol string
-//	host     string
-//	port     int
-//	path     string
-//}
-
 func resourceKongService() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceKongServiceCreate,
 		Read:   resourceKongServiceRead,
 		Update: resourceKongServiceUpdate,
 		Delete: resourceKongServiceDelete,
-
-		/* // not here: url property
-		{
-			"id": "4e13f54a-bbf1-47a8-8777-255fed7116f2", (computed)
-			"created_at": 1488869076800, (computed)
-			"updated_at": 1488869076800, (computed)
-			"connect_timeout": 60000, (optional)
-			"protocol": "http", (required, one of (http (default), https)
-			"host": "example.org", (required)
-			"port": 80, (required, default)
-			"path": "/api", (optional)
-			"name": "example-service", (optional)
-			"retries": 5, (optional, default)
-			"read_timeout": 60000 (optional, default),
-			"write_timeout": 60000 (optional, default)
-		}
-		*/
-
 		Schema: map[string]*schema.Schema{
 			"host": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
+				Required: false,
 			},
 			"path": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
+				Required: false,
 			},
 			"protocol": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
+				Required: false,
 			},
 			"name": &schema.Schema{
 				Type:     schema.TypeString,
